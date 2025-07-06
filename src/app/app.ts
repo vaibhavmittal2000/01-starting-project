8b1ea040197513c7fc7935059985d5dd7b9c9bfe
Vaibhav <vaibhavmittal2017@gmail.com>
2025-07-06 23:26:31 +0530

    Initialization of project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = '01-starting-project';
-}
+export class App {}
